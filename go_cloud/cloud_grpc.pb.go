58d71443e873dd1975779250947ced61ba6e7256
Softcorp Automation <dev@softcorp.io>
2022-05-31 12:32:14 +0000

    updated protobuf files

--- go_cloud/cloud_grpc.pb.go
+++ go_cloud/cloud_grpc.pb.go
@@ -21,7 +21,7 @@ type CloudServiceClient interface {
 	Heartbeat(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
 	CreateOrganization(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
 	GetOrganization(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
-	GetUsersOrganizations(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
+	GetUserOrganizations(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
 	UpdateOrganization(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
 	CreateProject(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
 	RollPrivateKey(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error)
@@ -69,9 +69,9 @@ func (c *cloudServiceClient) GetOrganization(ctx context.Context, in *CloudReque
 	return out, nil
 }
 
-func (c *cloudServiceClient) GetUsersOrganizations(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error) {
+func (c *cloudServiceClient) GetUserOrganizations(ctx context.Context, in *CloudRequest, opts ...grpc.CallOption) (*CloudResponse, error) {
 	out := new(CloudResponse)
-	err := c.cc.Invoke(ctx, "/CloudProject.CloudService/GetUsersOrganizations", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/CloudProject.CloudService/GetUserOrganizations", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -175,7 +175,7 @@ type CloudServiceServer interface {
 	Heartbeat(context.Context, *CloudRequest) (*CloudResponse, error)
 	CreateOrganization(context.Context, *CloudRequest) (*CloudResponse, error)
 	GetOrganization(context.Context, *CloudRequest) (*CloudResponse, error)
-	GetUsersOrganizations(context.Context, *CloudRequest) (*CloudResponse, error)
+	GetUserOrganizations(context.Context, *CloudRequest) (*CloudResponse, error)
 	UpdateOrganization(context.Context, *CloudRequest) (*CloudResponse, error)
 	CreateProject(context.Context, *CloudRequest) (*CloudResponse, error)
 	RollPrivateKey(context.Context, *CloudRequest) (*CloudResponse, error)
@@ -201,8 +201,8 @@ func (UnimplementedCloudServiceServer) CreateOrganization(context.Context, *Clou
 func (UnimplementedCloudServiceServer) GetOrganization(context.Context, *CloudRequest) (*CloudResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetOrganization not implemented")
 }
-func (UnimplementedCloudServiceServer) GetUsersOrganizations(context.Context, *CloudRequest) (*CloudResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetUsersOrganizations not implemented")
+func (UnimplementedCloudServiceServer) GetUserOrganizations(context.Context, *CloudRequest) (*CloudResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUserOrganizations not implemented")
 }
 func (UnimplementedCloudServiceServer) UpdateOrganization(context.Context, *CloudRequest) (*CloudResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateOrganization not implemented")
@@ -300,20 +300,20 @@ func _CloudService_GetOrganization_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
-func _CloudService_GetUsersOrganizations_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _CloudService_GetUserOrganizations_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CloudRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(CloudServiceServer).GetUsersOrganizations(ctx, in)
+		return srv.(CloudServiceServer).GetUserOrganizations(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/CloudProject.CloudService/GetUsersOrganizations",
+		FullMethod: "/CloudProject.CloudService/GetUserOrganizations",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(CloudServiceServer).GetUsersOrganizations(ctx, req.(*CloudRequest))
+		return srv.(CloudServiceServer).GetUserOrganizations(ctx, req.(*CloudRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -518,8 +518,8 @@ var CloudService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _CloudService_GetOrganization_Handler,
 		},
 		{
-			MethodName: "GetUsersOrganizations",
-			Handler:    _CloudService_GetUsersOrganizations_Handler,
+			MethodName: "GetUserOrganizations",
+			Handler:    _CloudService_GetUserOrganizations_Handler,
 		},
 		{
 			MethodName: "UpdateOrganization",
